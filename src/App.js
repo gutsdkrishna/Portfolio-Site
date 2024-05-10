f9c38ad73217dd6b81f8cb19d9a9436dbe449f4a
Krishna Paikine <119587231+gutsdkrishna@users.noreply.github.com>
2024-05-10 22:51:54 +0530

    Netli

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { ThemeProvider } from "styled-components";
-import { useState, useEffect } from "react";
+import { useState,  } from "react";
 import { lightTheme } from './utils/Themes.js'
 import Navbar from "./components/Navbar";
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HeroSection from "./components/HeroSection";
-import About from "./components/About";
 import Skills from "./components/Skills";
 //import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
-import ProjectDetails from "./components/ProjectDetails";
 import styled from "styled-components";
 
 const Body = styled.div`
